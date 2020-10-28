58a65a91fc3c6e86874d8964fb841db194d005f4
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-28 02:13:21 +0100

    Update MapDist.cpp

--- Struktury/MapDist.cpp
+++ Struktury/MapDist.cpp
@@ -2,13 +2,13 @@
 #include "MapPoint.h"
 #include "cmath"
 
-MapDist distance(MapPoint p1, MapPoint p2){//EOM tu nie może być const bo tworzymy obiekt
+MapDist distance(MapPoint p1, MapPoint p2){
   MapDist point;
   point.longitude = p1.longitude - p2.longitude;
   point.latitude = p1.latitude - p2.latitude;
   return point;
 }
 
-double angularDistance(const MapDist p){ //EOM EDIT const
+double angularDistance(const MapDist p){ 
   return sqrt(p.longitude*p.longitude + p.latitude*p.latitude);
 }
