8ce7ba1047daa697821396fbaa01be229b8b813c
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-28 11:02:53 +0100

    Update MapDist.cpp

--- Structures/MapDist.cpp
+++ Structures/MapDist.cpp
@@ -6,6 +6,12 @@ MapDist distance(MapPoint p1, MapPoint p2){
   MapDist point;
   point.longitude = p1.longitude - p2.longitude;
   point.latitude = p1.latitude - p2.latitude;
+  if(point.longitude < 0){
+    point.longitude += 2*(-point.longitude);
+  }
+  if(point.latitude < 0){
+    point.latitude += 2*(-point.latitude);
+  }
   return point;
 }
 
