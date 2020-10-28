3b8ee442ae000c58b0aaafce72e35df828983db6
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-28 02:14:04 +0100

    Update MapDist.h

--- Struktury/MapDist.h
+++ Struktury/MapDist.h
@@ -3,6 +3,6 @@
 struct MapDist{
   double longitude;
   double latitude;
-}; //dlaczego tu nie definicji struktury?
-MapDist distance(MapPoint, MapPoint);//EOM EDIT const
-double angularDistance(const MapDist);//EOM EDIT const
+}; 
+MapDist distance(MapPoint, MapPoint);
+double angularDistance(const MapDist);
