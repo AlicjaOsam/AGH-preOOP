eb3ebd977da5f0b4e3dc9edba551e261a468e255
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-28 02:15:59 +0100

    Update MapPoint.h

--- Struktury/MapPoint.h
+++ Struktury/MapPoint.h
@@ -6,15 +6,13 @@ struct MapPoint{
   double latitude;
   char directx;
   char directy;
-}; //dlaczego tu nie definicji struktury?
-MapPoint *construct(const char* , const double , const double);//EOM const char* const double
-MapPoint *construct(char* , double, char , double, char);//EOM const char* 
+}; 
+MapPoint *construct(const char* , const double , const double);
+MapPoint *construct(char* , double, char , double, char);
 void print(const MapPoint*);
 void movePoint(MapPoint* , const double, const double);
 MapPoint inTheMiddle(MapPoint* , const MapPoint*, char*);
 char* getName(const MapPoint);
 MapPoint closestPlace(MapPoint*, MapPoint*, MapPoint*);
-//void clear(MapPoint* , ...);
-
-void clear (MapPoint*);//EOM edit
+void clear (MapPoint*);
 void clear (MapPoint*,MapPoint*,MapPoint*);
