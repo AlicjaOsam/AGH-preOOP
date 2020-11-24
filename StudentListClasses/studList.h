04e1f70020ecf71a02d15367347030234f5d58bc
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-11-24 12:17:12 +0100

    Update studList.h

--- StudentListClasses/studList.h
+++ StudentListClasses/studList.h
@@ -5,34 +5,34 @@ class StudList;
 
 class Element {
   public:
-  friend class StudList;
-  Element(char* );
-  ~Element();
-  char* getName();
-  void printName();
+    friend class StudList;
+    Element(char* );
+    ~Element();
+    char* getName();
+    void printName();
   private:
-  char* _n;
-  Element *_next;
+    char* _n;
+    Element *_next;
 };
 
 class StudList {
   public:
-  StudList(char* );
-  ~StudList();
-  void prepend(char* );
-  void prepend(Element* );
-  Element* getHead();
-  Element* getTail();
-  char* getName() const;
-  bool isEmpty() const;
-  void print() const;
-  Element* getLast();
-  void clearList();
-  void removeLast();
+    StudList(char* );
+    ~StudList();
+    void prepend(char* );
+    void prepend(Element* );
+    Element* getHead();
+    Element* getTail();
+    char* getName() const;
+    bool isEmpty() const;
+    void print() const;
+    Element* getLast();
+    void clearList();
+    void removeLast();
   private:
-  char *_name;
-  Element* _head;
-  Element* _tail;
+    char *_name;
+    Element* _head;
+    Element* _tail;
 };
 
 char* mycopy (const char* string);
